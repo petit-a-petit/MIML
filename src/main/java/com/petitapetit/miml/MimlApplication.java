6b00c17463e51002e47a707a2ba2ce4bfe31e6a3
Jeonwoojae <dnwo0719@icloud.com>
2023-09-25 18:29:06 +0900

    init project🎉

---
+++ src/main/java/com/petitapetit/miml/MimlApplication.java
@@ -0,0 +1,13 @@
+package com.petitapetit.miml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MimlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petitapetit/miml/MimlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petitapetit.miml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
