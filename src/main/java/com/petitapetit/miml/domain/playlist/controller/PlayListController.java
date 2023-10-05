425175d1be865f1578fe4e67508b40a976840ed7
kdmstj <rkddmstj429@gmail.com>
2023-10-05 22:10:59 +0900

    feat: 컨트롤러 POST 플레이리스트 등록(postPlayList 메소드)생성
    - 컨트롤러에서 postPlayList 메서드를 구현하였고, 테스트를 통과하기 위해서 HttpStatus ok를 리턴하였습니다.

--- src/main/java/com/petitapetit/miml/domain/playlist/controller/PlayListController.java
+++ src/main/java/com/petitapetit/miml/domain/playlist/controller/PlayListController.java
@@ -1,7 +1,19 @@
 package com.petitapetit.miml.domain.playlist.controller;
 
+import com.petitapetit.miml.domain.playlist.dto.PlayListDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/playlists")
 public class PlayListController {
+
+    @PostMapping
+    public ResponseEntity postPlayList(@RequestBody PlayListDto.Post requestBody){
+
+        return ResponseEntity.ok().build();
+    }
 }
