eef79a1faae01053233a0014f5421e1bdea10f8a
kdmstj <rkddmstj429@gmail.com>
2023-10-11 23:07:38 +0900

    fix: 플레이리스트 공개 여부 변수명 변경
    - 플레이리스트 공개 여부 변수명에 대해 boolean 변수이므로 be동사 is를 사용하여 ture/false를 표현하는것이 좋다고 생각되어 변경되었습니다.

--- src/main/java/com/petitapetit/miml/domain/playlist/dto/PlayListDto.java
+++ src/main/java/com/petitapetit/miml/domain/playlist/dto/PlayListDto.java
@@ -2,13 +2,12 @@ package com.petitapetit.miml.domain.playlist.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 public class PlayListDto {
 
     @AllArgsConstructor @NoArgsConstructor
     public static class Post {
         private String name;
-        private boolean publicYN;
+        private boolean isPublic;
     }
 }
