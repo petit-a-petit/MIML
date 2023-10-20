33196d1d1e035a91d4750118d3e64a0926e5d551
minju412 <59405576+minju412@users.noreply.github.com>
2023-10-20 22:29:53 +0900

    refactor: 불필요한 코드 제거
    - 트랜잭션 관리가 필요하지 않은 부분이기 때문에 제거

--- src/main/java/com/petitapetit/miml/domain/member/repository/MemberRepository.java
+++ src/main/java/com/petitapetit/miml/domain/member/repository/MemberRepository.java
@@ -4,13 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.petitapetit.miml.domain.member.model.Member;
 import com.petitapetit.miml.domain.member.model.MemberId;
 
 @Repository
-@Transactional
 public interface MemberRepository extends JpaRepository<Member, MemberId> {
 	Optional<Member> findById(MemberId id);
 }
