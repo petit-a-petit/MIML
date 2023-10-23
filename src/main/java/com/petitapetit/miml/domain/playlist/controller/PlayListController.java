c638c352886f719f99b8b4e73c9f2255fd3e6d63
kdmstj <rkddmstj429@gmail.com>
2023-10-23 16:55:15 +0900

    feat: Controller 플레이리스트 수정/조회/삭제 엔드포인트 구현
    - 특정 플레이리스트에 접근해야 하는 경우 (수정, 특정 플레이리스트 조회, 삭제) @PathVariable 을 사용하였습니다.
    - ResponseEntity 로 HttpStatus 코드와 Body 를 반환하였습니다.

--- src/main/java/com/petitapetit/miml/domain/playlist/controller/PlayListController.java
+++ src/main/java/com/petitapetit/miml/domain/playlist/controller/PlayListController.java
@@ -2,12 +2,13 @@ package com.petitapetit.miml.domain.playlist.controller;
 
 import com.petitapetit.miml.domain.playlist.dto.PlayListDto;
 import com.petitapetit.miml.domain.playlist.service.PlayListService;
+import com.petitapetit.miml.util.UriUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/playlists")
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class PlayListController {
 
     private final PlayListService playListService;
+    public static final String DEFAULT_URI = "/playlists";
+
 
     @PostMapping
     public ResponseEntity<PlayListDto.SaveResponse> postPlayList(@RequestBody PlayListDto.SaveRequest request){
@@ -22,6 +25,43 @@ public class PlayListController {
         Long memberId = 1L; //mock
         PlayListDto.SaveResponse response = playListService.savePlayList(request , memberId);
 
+        URI uri = UriUtil.createUri(DEFAULT_URI, response.getPlayListId());
+
+        return ResponseEntity.created(uri).body(response);
+    }
+
+    @PatchMapping("/{play_list_id}")
+    public ResponseEntity<PlayListDto.SaveResponse> patchPlayList(@PathVariable("play_list_id") Long playListId,
+                                                                  @RequestBody PlayListDto.SaveRequest request){
+
+        Long memberId = 1L;//mock
+        PlayListDto.SaveResponse response = playListService.modifyPlayList(playListId, request, memberId);
+
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{play_list_id}")
+    public ResponseEntity<PlayListDto.Response> getPlayListDetail(@PathVariable("play_list_id") Long playListId){
+
+        PlayListDto.Response response = playListService.getPlayListById(playListId);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PlayListDto.Response>> getPlayLists(){
+
+        List<PlayListDto.Response> response = playListService.getPlayLists();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{play_list_id}")
+    public ResponseEntity<String> deletePlayList(@PathVariable("play_list_id") Long playListId){
+
+        Long memberId = 1L;
+        playListService.deletePlayList(playListId, memberId);
+
+        return ResponseEntity.noContent().build();
     }
 }
