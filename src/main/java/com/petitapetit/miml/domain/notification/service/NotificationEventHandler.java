10a00ec8374fa268e1688b29b85549a41a953044
Jeonwoojae <dnwo0719@icloud.com>
2023-10-24 01:28:13 +0900

    test: 알림 트랜잭션 세분화 테스트 코드 작성
    - @TransactionalEventListener를 AFTER_COMMIT 설정 시 이미 앞에서 COMMIT이 완료되어 DB에 접근할 수 없는 문제를 REQUIRES_NEW 옵션으로 해결
    - 음악 추가 이벤트 처리 시 사용자 별로 알림을 보내는 트랜잭션을 적용하기 위해 알림 전소 함수 분리
    - Cascade 옵션으로 테스트 시 에러 발생, 꼭 필요하지 않기 때문에 TempUser에서 Cascade 옵션 삭제

--- src/main/java/com/petitapetit/miml/domain/notification/service/NotificationEventHandler.java
+++ src/main/java/com/petitapetit/miml/domain/notification/service/NotificationEventHandler.java
@@ -10,16 +10,19 @@ import com.petitapetit.miml.domain.notification.entity.SongAddedNotification;
 import com.petitapetit.miml.domain.notification.event.*;
 import com.petitapetit.miml.domain.notification.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-import javax.transaction.Transactional;
 import java.util.*;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+//@Async("asyncExecutor") // 비동기로 처리!
 public class NotificationEventHandler {
 
     private final TempUserRepository userRepository;
@@ -27,26 +30,42 @@ public class NotificationEventHandler {
     private final MailService mailService;
 
     @TransactionalEventListener(classes = SongAddedEvent.class, phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void handleSongEvent(SongAddedEvent event) {
+        log.info("이벤트 감지 : {}",event);
+        log.info("현재 스레드 정보 : {}", Thread.currentThread());
         TempSong song = event.getSong();
 
         Set<TempUser> users = userRepository.findByLikeArtistsSetContaining(song.getArtist());
 
         for (TempUser user : users) {
-            SongAddedNotification notification = new SongAddedNotification(user, song);
-            mailService.sendEmail(notification);
-            notificationRepository.save(notification);
+            sendToUserAboutNewSongNotification(song, user);
         }
     }
 
+    @Transactional(propagation = Propagation.NESTED)
+    public void sendToUserAboutNewSongNotification(TempSong song, TempUser user) {
+        SongAddedNotification notification = new SongAddedNotification(user, song);
+        mailService.sendEmail(notification);
+        notificationRepository.save(notification);
+    }
+
     @TransactionalEventListener(classes = FriendRequestedEvent.class, phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void handleFriendRequestEvent(FriendRequestedEvent event) {
+        log.debug("이벤트 감지 : {}",event);
+        log.debug("현재 스레드 정보 : {}", Thread.currentThread());
+
         FriendRequestedNotification notification = new FriendRequestedNotification(event.getCurrentUserName(), event.getRequestedUserName());
         mailService.sendEmail(notification);
         notificationRepository.save(notification);
     }
     @TransactionalEventListener(classes = SharePlaylistRequestedEvent.class, phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void handleSharePlaylistRequestEvent(SharePlaylistRequestedEvent event) {
+        log.debug("이벤트 감지 : {}",event);
+        log.debug("현재 스레드 정보 : {}", Thread.currentThread());
+
         SharePlaylistRequestedNotification notification = new SharePlaylistRequestedNotification(event.getCurrentUserEmail(), event.getRequestedUserEmail());
         mailService.sendEmail(notification);
         notificationRepository.save(notification);
