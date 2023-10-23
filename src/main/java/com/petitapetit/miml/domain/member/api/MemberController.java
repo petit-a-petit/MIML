0d500210a976ab17c7bc0b92c7726d811d637a1e
minju412 <59405576+minju412@users.noreply.github.com>
2023-10-23 15:17:04 +0900

    refactor: 서브모듈 적용
    - 설정 파일의 변경 사항이 생길 때마다 내용을 공유하지 않아도 작동하게끔 서브모듈 적용

--- src/main/java/com/petitapetit/miml/domain/member/api/MemberController.java
+++ src/main/java/com/petitapetit/miml/domain/member/api/MemberController.java
@@ -1,5 +1,6 @@
 package com.petitapetit.miml.domain.member.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -7,21 +8,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.client.RestTemplate;
 
 import com.petitapetit.miml.domain.auth.oauth.CustomOAuth2User;
 
+
 @Controller
 public class MemberController {
 
-	// @Value("${spotify.api.url}")
-	private String spotifyApiUrl = "https://api.spotify.com"; // Spotify API 엔드포인트 URL
+	@Value("${spotify.host}")
+	private String SpotifyHost;
+
+	@Value("${spotify.endpoint.me}")
+	private String myDataEndpoint;
 
 	@GetMapping("/me")
 	public ResponseEntity<String> getMySpotifyData(@AuthenticationPrincipal CustomOAuth2User oAuth2User) {
 
-		String apiUrl = spotifyApiUrl + "/v1/me";
+		String apiUrl = SpotifyHost + myDataEndpoint;
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization", "Bearer " + oAuth2User.getAccessToken());
