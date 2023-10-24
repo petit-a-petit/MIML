12551c457e92a465128a08b3b354df33efc7be38
minju412 <59405576+minju412@users.noreply.github.com>
2023-10-24 22:00:24 +0900

    refactor: @UtilityClass 어노테이션 사용
    - 생성자를 가질 수 없도록 막고 모든 메서드를 정적으로 정의
    - 보다 직관적으로 유티리티 클래스임을 명시

--- src/main/java/com/petitapetit/miml/global/util/SessionUtil.java
+++ src/main/java/com/petitapetit/miml/global/util/SessionUtil.java
@@ -2,24 +2,23 @@ package com.petitapetit.miml.global.util;
 
 import javax.servlet.http.HttpSession;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class SessionUtil {
 	private static final String ORIGINAL_REQUEST_URI = "ORIGINAL_REQUEST_URI";
-
-	// 인스턴스화 방지
-	private SessionUtil() {
-	}
 
 	/**
 	 * 로그인을 시도한 유저가 접속하고자 하는 ORIGINAL URI 를 세션에 저장한다.
 	 */
-	public static void setOriginalRequestUri(HttpSession session, String originalRequestUri) {
+	public void setOriginalRequestUri(HttpSession session, String originalRequestUri) {
 		session.setAttribute(ORIGINAL_REQUEST_URI, originalRequestUri);
 	}
 
 	/**
 	 * 로그인한 유저가 접속하고자 했던 ORIGINAL URI 를 세션에서 꺼낸다.
 	 */
-	public static String getOriginalRequestUri(HttpSession session) {
+	public String getOriginalRequestUri(HttpSession session) {
 		return (String) session.getAttribute(ORIGINAL_REQUEST_URI);
 	}
 
