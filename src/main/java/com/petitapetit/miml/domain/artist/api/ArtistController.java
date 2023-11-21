1fcb03108dc9bb8aaefba6323cacb9a44b237364
Jeonwoojae <dnwo0719@icloud.com>
2023-11-22 00:28:21 +0900

    feat: 아티스트 좋아요 관련 기능 컨트롤러 구현

---
+++ src/main/java/com/petitapetit/miml/domain/artist/api/ArtistController.java
@@ -0,0 +1,54 @@
+package com.petitapetit.miml.domain.artist.api;
+
+import com.petitapetit.miml.domain.artist.dto.ArtistDto;
+import com.petitapetit.miml.domain.artist.dto.ArtistDto.Response;
+import com.petitapetit.miml.domain.artist.service.ArtistService;
+import com.petitapetit.miml.domain.auth.oauth.CustomOAuth2User;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/artist")
+@RequiredArgsConstructor
+public class ArtistController {
+
+    private final ArtistService artistService;
+
+    // 아티스트 좋아요
+    @PostMapping("/like/{artistId}")
+    public ResponseEntity<Void> likeArtist(
+            @PathVariable Long artistId,
+            @AuthenticationPrincipal CustomOAuth2User oAuth2User
+    ) {
+        artistService.likeArtist(oAuth2User.getUser().getMemberId(), artistId);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    // 사용자가 좋아요한 아티스트 목록 조회
+    @GetMapping("/liked")
+    public ResponseEntity<List<Response>> getLikedArtistList(
+            @AuthenticationPrincipal CustomOAuth2User oAuth2User
+    ) {
+        List<ArtistDto.Response> likedArtists = artistService.getLikedArtists(oAuth2User.getUser().getMemberId());
+        return ResponseEntity.status(HttpStatus.OK).body(likedArtists);
+    }
+
+    // 아티스트 좋아요 취소
+    @DeleteMapping("/like/{artistId}")
+    public ResponseEntity<Void> cancelLikeArtist(
+            @PathVariable Long artistId,
+            @AuthenticationPrincipal CustomOAuth2User oAuth2User
+    ) {
+        artistService.cancelLikeArtist(oAuth2User.getUser().getMemberId(), artistId);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+}
