0bfd2e2aabf32633bcec4097ac03cba40da155eb
Jeonwoojae <dnwo0719@icloud.com>
2023-11-24 06:15:10 +0900

    feat: artist와 track, member와 artist 연관된 객체 조회 기능 구현
    - fetch join과 @EntityGraph 방식 사용하며 사용방법 비교

--- src/main/java/com/petitapetit/miml/domain/artist/domain/MemberArtistRepository.java
+++ src/main/java/com/petitapetit/miml/domain/artist/domain/MemberArtistRepository.java
@@ -1,8 +1,10 @@
 package com.petitapetit.miml.domain.artist.domain;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberArtistRepository extends JpaRepository<MemberArtist, Long> {
+    @EntityGraph(attributePaths = {"member", "artist"})
     Optional<MemberArtist> findByMember_MemberIdAndArtist_Id(Long memberId, Long artistId);
 }
--- src/main/java/com/petitapetit/miml/domain/track/repository/ArtistTrackRepository.java
+++ src/main/java/com/petitapetit/miml/domain/track/repository/ArtistTrackRepository.java
@@ -1,7 +1,12 @@
 package com.petitapetit.miml.domain.track.repository;
 
 import com.petitapetit.miml.domain.track.entity.ArtistTrack;
+import com.petitapetit.miml.domain.track.entity.Track;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ArtistTrackRepository extends JpaRepository<ArtistTrack, Long> {
+    @Query("select at from ArtistTrack at join fetch at.artist where at.track = :track")
+    List<ArtistTrack> findAllByTrack(Track track);
 }
