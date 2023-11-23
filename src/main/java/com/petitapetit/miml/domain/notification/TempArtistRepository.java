7b2f4c071a4ee4198423dfa0b3d28d5933081e81
Jeonwoojae <dnwo0719@icloud.com>
2023-11-23 21:15:33 +0900

    refactor: 사용하지 않는 임시 객체들 삭제
    - Temp 관련 임시 객체들 삭제

--- src/main/java/com/petitapetit/miml/domain/notification/TempArtistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.petitapetit.miml.domain.notification;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TempArtistRepository extends JpaRepository<TempArtist, Long> {
-    TempArtist findByName(String name);
-}
