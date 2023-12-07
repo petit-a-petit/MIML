7174d2fcd0ac947010aa4ea5e63665846f4aa0bf
Jeonwoojae <dnwo0719@icloud.com>
2023-12-07 22:43:54 +0900

    test: 플레이리스트에서 음악 추가, 삭제 테스트
    - 단위 테스트를 진행하여 mocking을 통해 save나 delete메서드를 호출하는지 확인

--- src/test/java/com/petitapetit/miml/domain/playlist/PlayListServiceTest.java
+++ src/test/java/com/petitapetit/miml/domain/playlist/PlayListServiceTest.java
@@ -2,9 +2,14 @@ package com.petitapetit.miml.domain.playlist;
 
 import com.petitapetit.miml.domain.playlist.dto.PlayListDto;
 import com.petitapetit.miml.domain.playlist.entity.PlayList;
+import com.petitapetit.miml.domain.playlist.entity.TrackPlayList;
 import com.petitapetit.miml.domain.playlist.mapper.PlayListMapper;
 import com.petitapetit.miml.domain.playlist.repository.PlayListRepository;
+import com.petitapetit.miml.domain.playlist.repository.TrackPlayListRepository;
 import com.petitapetit.miml.domain.playlist.service.PlayListService;
+import com.petitapetit.miml.domain.track.entity.Track;
+import com.petitapetit.miml.domain.track.repository.TrackRepository;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,10 +18,16 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class PlayListServiceTest {
+class PlayListServiceTest {
 
     @InjectMocks
     private PlayListService playListService;
@@ -26,10 +37,14 @@ public class PlayListServiceTest {
 
     @Mock
     private PlayListRepository playListRepository;
+    @Mock
+    private TrackRepository trackRepository;
+    @Mock
+    private TrackPlayListRepository trackPlayListRepository;
 
     @DisplayName("서비스 플레이리스트 등록 로직 확인")
     @Test
-    public void savePlayList(){
+    void savePlayList(){
         //given
         PlayListDto.SaveRequest saveRequest = PlayListDto.SaveRequest.builder()
                 .name("플레이리스트 이름")
@@ -58,5 +73,46 @@ public class PlayListServiceTest {
         //then
         assertThat(savedPlayList.getPlayListId()).isEqualTo(1L);
 
+    }
+
+    @Test
+    @DisplayName("플레이리스트에 음악 추가 성공")
+    void addTrackToPlayList() {
+        PlayList playList = PlayList.builder()
+                .playListId(1L)
+                .memberId(1L)
+                .name("playList")
+                .isPublic(true)
+                .build();
+
+        Track track = new Track(1L,1,"url","title","source",null,null);
+
+        when(playListRepository.findById(anyLong())).thenReturn(Optional.of(playList));
+        when(trackRepository.findById(anyLong())).thenReturn(Optional.of(track));
+
+        playListService.addTrackToPlayList(1L, 1L, 1L);
+
+        verify(trackPlayListRepository, times(1)).save(any(TrackPlayList.class));
+    }
+
+    @Test
+    @DisplayName("플레이리스트에서 음악 제거 성공")
+    void removeTrackFromPlayList() {
+        PlayList playList = PlayList.builder()
+                .playListId(1L)
+                .memberId(1L)
+                .name("playList")
+                .isPublic(true)
+                .build();
+
+        Track track = new Track(1L,1,"url","title","source",null,null);
+        TrackPlayList trackPlayList = new TrackPlayList(1L,track,playList);
+
+        when(playListRepository.findById(anyLong())).thenReturn(Optional.of(playList));
+        when(trackPlayListRepository.findByTrackIdAndPlayListPlayListId(anyLong(), anyLong())).thenReturn(Optional.of(trackPlayList));
+
+        playListService.removeTrackFromPlayList(1L, 1L, 1L);
+
+        verify(trackPlayListRepository, times(1)).delete(trackPlayList);
     }
 }
