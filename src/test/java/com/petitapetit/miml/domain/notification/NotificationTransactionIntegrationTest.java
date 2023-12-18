05371659d439fe0493cc1a962e3e389f04d51455
Jeonwoojae <dnwo0719@icloud.com>
2023-12-18 21:58:05 +0900

    test: 통합 테스트 시 DirtiesContext 사용하지 않도록 변경
    - DirtiesContext를 사용하지 않기 위해 테스트 과정엣 사용했던 객체 정보만 테스트 종료 시 db에서 삭제
    - 인스턴스 변수명을 내부에서 선언하여 삭제할 수 있도록 수정
    - 테스트 코드 초기화를 위한 코드 deleteByArtist, deleteByTrack, deleteByUserEmail 작성

--- src/test/java/com/petitapetit/miml/domain/notification/NotificationTransactionIntegrationTest.java
+++ src/test/java/com/petitapetit/miml/domain/notification/NotificationTransactionIntegrationTest.java
@@ -2,6 +2,7 @@ package com.petitapetit.miml.domain.notification;
 
 import com.petitapetit.miml.domain.artist.domain.Artist;
 import com.petitapetit.miml.domain.artist.domain.ArtistRepository;
+import com.petitapetit.miml.domain.artist.service.ArtistService;
 import com.petitapetit.miml.domain.auth.oauth.OAuth2Provider;
 import com.petitapetit.miml.domain.mail.serivce.MailService;
 import com.petitapetit.miml.domain.member.model.Member;
@@ -12,9 +13,11 @@ import com.petitapetit.miml.domain.notification.entity.Notification;
 import com.petitapetit.miml.domain.notification.repository.NotificationRepository;
 import com.petitapetit.miml.domain.track.entity.Track;
 import com.petitapetit.miml.domain.track.dto.TrackDto;
+import com.petitapetit.miml.domain.track.repository.ArtistTrackRepository;
 import com.petitapetit.miml.domain.track.repository.TrackRepository;
 import com.petitapetit.miml.domain.track.service.TrackService;
 import java.util.Set;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -23,16 +26,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
-import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
 
 @SpringBootTest(properties = {"spring.batch.job.enabled=false"})
-@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
 class NotificationTransactionIntegrationTest {
     @Autowired
     private TrackRepository trackRepository;
@@ -43,16 +43,42 @@ class NotificationTransactionIntegrationTest {
     @Autowired
     private ArtistRepository artistRepository;
     @Autowired
+    private ArtistTrackRepository artistTrackRepository;
+    @Autowired
     private MemberService memberService;
+    @Autowired
+    private ArtistService artistService;
     @MockBean
     private MailService mailService;
     @Autowired
     private NotificationRepository notificationRepository;
+
+    private Artist artist;
+    private Member member;
+    private Track track;
+
+    @AfterEach
+    void tearDown() {
+        // 사용자와 아티스트 간 관계 제거
+        artistService.cancelLikeArtist(member.getMemberId(),artist.getId());
+        if(member != null) member.getLikedArtists().clear();
+        if(artist != null) artist.getLikedByUsers().clear();
+        // 노래와 아티스트 간 관계 제거
+        if(artist != null) artist.getArtistTracks().clear();
+        if(track != null) track.getArtistTracks().clear();
+        artistTrackRepository.deleteByArtist(artist);
+        artistTrackRepository.deleteByTrack(track);
+
+        notificationRepository.deleteByUserEmail(member.getEmail());
+        if(artist != null) artistRepository.delete(artist);
+        if(member != null) memberRepository.delete(member);
+        if(track != null) trackRepository.delete(track);
+    }
     @Test
     @DisplayName("아티스트로 좋아요한 사용자 찾기")
     void testFindMembersByLikedArtists() {
         // 멤버 생성
-        Member member = Member.builder()
+        member = Member.builder()
                 .name("Test User")
                 .email("test@example.com")
                 .role(RoleType.ROLE_USER)
@@ -62,11 +88,11 @@ class NotificationTransactionIntegrationTest {
         memberRepository.save(member);
 
         String artistName = "Artist1";
-        Artist artist = new Artist(artistName);
+        artist = new Artist(artistName);
         artist = artistRepository.save(artist);
 
         TrackDto dto = new TrackDto(1,"spotify:url",artistName,"song","JYP","2","1","1","100");
-        Track track = new Track(dto);
+        track = new Track(dto);
         track = trackRepository.save(track);
 
         memberService.likeArtist(member,artist.getId());
@@ -85,15 +111,29 @@ class NotificationTransactionIntegrationTest {
         @DisplayName("음악 생성이 실패 시 이벤트가 발행되지 않아야 한다.")
         void testNoEventOnSongCreationFailure() {
             // given: 유저 생성, 아티스트 이름과 노래 이름 설정
-            TrackDto dto = new TrackDto(1,"spotify:url",null,"trackName","JYP","2","1","1","100");
+            String artistName = "Artist1";
+            artist = new Artist(artistName);
+            artist = artistRepository.save(artist);
+
+            TrackDto dto = new TrackDto(1,"spotify:url",artistName,"song","JYP","2","1","1","100");
+
+            member = Member.builder()
+                    .name("Test User")
+                    .email("test@example.com")
+                    .role(RoleType.ROLE_USER)
+                    .provider(OAuth2Provider.SPOTIFY)
+                    .providerId("test")
+                    .build();
+            member = memberRepository.save(member);
+            memberService.likeArtist(member,artist.getId());
 
             // when: addNewSong 메소드 호출하지만 내부 예외 발생
-            assertThrows(IllegalArgumentException.class,
+            assertThrows(NullPointerException.class,
                     () -> trackService.addNewSong(dto,null));
 
             // then: 저장된 곡과 알림이 없음을 확인
             List<Track> songs = trackRepository.findByName("trackName");
-            List<Notification> notifications = notificationRepository.findAll();
+            List<Notification> notifications = notificationRepository.findAllByUserEmail(member.getEmail());
             assertTrue(songs.isEmpty());
             assertTrue(notifications.isEmpty());
         }
@@ -103,12 +143,12 @@ class NotificationTransactionIntegrationTest {
         void testNotificationFailure() {
             // given: 아티스트와 노래, 그 아티스트를 좋아하는 유저 생성
             String artistName = "Artist1";
-            Artist artist = new Artist(artistName);
+            artist = new Artist(artistName);
             artist = artistRepository.save(artist);
 
             TrackDto dto = new TrackDto(1,"spotify:url",artistName,"song","JYP","2","1","1","100");
 
-            Member member = Member.builder()
+            member = Member.builder()
                     .name("Test User")
                     .email("test@example.com")
                     .role(RoleType.ROLE_USER)
@@ -121,7 +161,7 @@ class NotificationTransactionIntegrationTest {
             doThrow(new RuntimeException()).when(mailService).sendEmail(any(Notification.class));
 
             // when: 신곡 추가 이벤트가 발생하고 음악이 저장되었음을 확인
-            trackService.addNewSong(dto, List.of(artist));
+            track = trackService.addNewSong(dto, List.of(artist));
 
             // then : 음악은 저장되고 알림은 저장
             List<Track> songs = trackRepository.findByName("song");
@@ -135,12 +175,12 @@ class NotificationTransactionIntegrationTest {
         void testEventOnSongCreationSuccess() {
             // given: 유저 생성, 아티스트 이름과 노래 이름 설정
             String artistName = "Artist1";
-            Artist artist = new Artist(artistName);
+            artist = new Artist(artistName);
             artist = artistRepository.save(artist);
 
             TrackDto dto = new TrackDto(1,"spotify:url",artistName,"song","JYP","2","1","1","100");
 
-            Member member = Member.builder()
+            member = Member.builder()
                     .name("Test User")
                     .email("test@example.com")
                     .role(RoleType.ROLE_USER)
@@ -151,7 +191,7 @@ class NotificationTransactionIntegrationTest {
             memberService.likeArtist(member,artist.getId());
 
             // when: addNewSong 메소드 호출
-            trackService.addNewSong(dto, List.of(artist));
+            track = trackService.addNewSong(dto, List.of(artist));
 
             // then: 신곡 추가 이벤트가 발생했음을 확인
             List<Track> songs = trackRepository.findAll();
